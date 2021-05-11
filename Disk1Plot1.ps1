482cf36cbe0e50ad086550eb6634bf7d6c32627c
nezhkind <dnezhkin@gmail.com>
2021-05-11 11:55:43 +0300

    internal commit

--- Disk1Plot1.ps1
+++ Disk1Plot1.ps1
@@ -1,3 +1,3 @@
 #Begin Script
 ############Plotters for Temp Drive###############
-invoke-expression 'cmd /c start powershell -NoExit -Command {cd "C:\Users\dnezh\AppData\Local\chia-blockchain\app-1.1.5\resources\app.asar.unpacked\daemon\";$host.ui.RawUI.WindowTitle = "Disk1Plot1"; start-sleep 0 ; .\chia.exe plots create -k 32 -b 5000 -u 128 -r 2 -t C:\CHIA\tmp -d E:\ -n 2 ;Read-Host -Prompt "press enter to exit"}'
+invoke-expression 'cmd /c start powershell -NoExit -Command {cd "C:\Users\Administrator\AppData\Local\chia-blockchain\app-1.1.5\resources\app.asar.unpacked\daemon\";$host.ui.RawUI.WindowTitle = "Disk1Plot1"; start-sleep 0 ; .\chia.exe plots create -k 32 -b 5000 -u 128 -r 2 -t C:\chia\tmp -d C:\chia -n 30 ;Read-Host -Prompt "press enter to exit"}'
